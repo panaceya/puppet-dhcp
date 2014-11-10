2f88cdd2ffd119249d557fb73bb441793befef54
Administrator <admin@example.com>
2014-11-10 16:38:31 +0200

    f

--- manifests/host.pp
+++ manifests/host.pp
@@ -9,6 +9,7 @@ define dhcp::host (
   include dhcp::params
 
   $dhcp_dir = $dhcp::params::dhcp_dir
+  $domain   = $dhcp::params::dnsdomain
 
   concat_fragment { "dhcp.hosts+10_${name}.hosts":
     content => template('dhcp/dhcpd.host.erb'),
